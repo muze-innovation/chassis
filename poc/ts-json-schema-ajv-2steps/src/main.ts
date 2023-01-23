8a64a1432d53ccfcec0d2a9b1a306bf9f6281a3a
Suwatcharin Issariyakasem <suwatcharinissariyakasem@SuwatcharinIssariyakasem-MacBook-Pro.local>
2023-01-23 11:52:10 +0700

    test: add 2 test cases and fix: all specs

--- poc/ts-json-schema-ajv-2steps/src/main.ts
+++ poc/ts-json-schema-ajv-2steps/src/main.ts
@@ -6,11 +6,11 @@ import { generator } from "./TJSconfig";
 // Declare AJV that used to validate between schema and data.
 const ajv = new Ajv();
 
-// Validate Basic Field (Not nested field)
-function validateBasic(): boolean {
+// Validate Basic Field (Not including nested field)
+function validateBasic(jsonData: {}): boolean {
   const basicSpec = generator.getSchemaForSymbol("Spec");
   const basicValidate = ajv.compile(basicSpec);
-  const basicValid = basicValidate(data);
+  const basicValid = basicValidate(jsonData);
 
   if (!basicValid) console.log(basicValidate.errors);
   return basicValid;
@@ -18,9 +18,10 @@ function validateBasic(): boolean {
 
 // Validate by viewType with payload type "static" or "remote".
 // If it's remote, then check that method and if it's static, then will check data type instead.
-function validatePayload(): void {
-  const allShelfFromData = data.items;
+function validatePayload(jsonData: any) {
+  const allShelfFromData = jsonData.items;
 
+  // Loop to validate each payload in shelf.
   for (const shelfData of allShelfFromData) {
     const viewType = shelfData.viewType;
 
@@ -34,9 +35,13 @@ function validatePayload(): void {
 
       if (!valid)
         console.log(`${JSON.stringify(validate.errors, null, 2)}
-      Error ${shelfData.id} from ${viewType}`);
+      Error in '${shelfData.id}' id and '${viewType}' viewType`);
+
+      return valid;
     }
   }
 }
 
-if (validateBasic()) validatePayload();
+if (validateBasic(data)) validatePayload(data);
+
+export { validateBasic, validatePayload };
