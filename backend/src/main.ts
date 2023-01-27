264b3354bf648049788eada8efdcbb7598b91ad4
thain.s muze <thain.s@muze.co.th>
2023-01-27 10:36:08 +0700

    export default bootstrap func for another workspace

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,5 +2,21 @@ import Chassis from './handler/Chassis'
 import { resolve } from 'path'
 import json from '../data/source.json'
 
-const chassis = new Chassis([resolve('./src/ViewSpec.ts'), resolve('./src/ResolverSpec.ts')])
-chassis.validateSpec(json)
+function bootstrap(specPaths?: string[]) {
+  // initial spec path
+  let specPathResolvers: string[] | [] = []
+
+  // spec path guard
+  if (specPaths && specPaths.length) {
+    // loop spec path with resolve path
+    specPathResolvers = specPaths.map(path => resolve(path))
+  }
+
+  const chassis = new Chassis([...specPathResolvers, resolve('./src/ViewSpec.ts'), resolve('./src/ResolverSpec.ts')])
+  chassis.validateSpec(json)
+}
+
+// TODO:: This execute only develop mode when deploy will remove this
+bootstrap()
+
+export default bootstrap
