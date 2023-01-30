177c544829354ec15766c2534c1f9811402bb2ee
Patrika Preda <patrika@muze.co.th>
2023-01-30 18:46:58 +0700

    Banner Widget (wip)

--- flutter_example_application/view_provider/lib/src/view_provider_base.dart
+++ flutter_example_application/view_provider/lib/src/view_provider_base.dart
@@ -1,18 +1,33 @@
 import 'package:chassis/interface.dart';
 import 'package:flutter/widgets.dart';
+import 'package:view_provider/example/banner.dart' as custom;
+
+// class ViewProvider implements IViewProvider {
+//   @override
+//   Widget getView(Stream<dynamic> stream, Map<String, dynamic> config) {
+//     return StreamBuilder(
+//         stream: stream,
+//         builder: (context, snapshot) {
+//           switch (snapshot.connectionState) {
+//           }
+//           print('snapshot.connectionState: ${snapshot.connectionState}');
+//           print('snapshot.data: ${snapshot.data}');
+//           print('snapshot.error: ${snapshot.error}');
+//           return const Text('data');
+//         });
+//   }
+// }
 
 class ViewProvider implements IViewProvider {
   @override
-  Widget getView(Stream<dynamic> stream, Map<String, dynamic> config) {
-    return StreamBuilder(
-        stream: stream,
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-          }
-          print('snapshot.connectionState: ${snapshot.connectionState}');
-          print('snapshot.data: ${snapshot.data}');
-          print('snapshot.error: ${snapshot.error}');
-          return const Text('data');
-        });
+  Widget? getView(Stream<dynamic> stream, Map<String, dynamic> config) {
+    var viewType = config['viewType'];
+    print('FoodViewProvider: $viewType');
+    switch (viewType) {
+      case 'Banner':
+        return custom.Banner(stream: stream, config: config);
+      default:
+        return null;
+    }
   }
 }
