c0ab9068167d71e8ba45fe16832781600c329710
thain.s muze <thain.s@muze.co.th>
2023-01-30 15:59:18 +0700

    mv dir example

--- backend/example/src/index.ts
+++ backend/example/src/index.ts
@@ -2,13 +2,17 @@ import Chassis from "chassis";
 import { resolve } from 'path'
 
 async function bootstrap() {
+  // Initial Instance Chassis
   const chassis = new Chassis([
     resolve(__dirname, './ViewSpec.ts'),
     resolve(__dirname, './ResolverSpec.ts')
   ])
 
-  const isValid = await chassis.validateSpec(resolve(__dirname, '../source.json'))
-  console.log(isValid)
+  // Generate json schema method.
+  console.log(await chassis.generateJsonSchema("Banner"))
+
+  // Valdiate spec
+  await chassis.validateSpec(resolve(__dirname, '../source.json'))
 }
 
 bootstrap()
