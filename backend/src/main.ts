92a414cbdee4c6161a267e6024ff79ecbeb9f63b
thain.s muze <thain.s@muze.co.th>
2023-01-31 10:57:44 +0700

    fix code bug

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,27 +1,7 @@
 import Chassis from './handler/Chassis'
-import { resolve } from 'path'
 import { ChassisViewSpec } from '../src/spec/ChassisViewSpec'
 import { ChassisResolverSpec } from '../src/spec/ChassisResolverSpec'
 
 export default Chassis
 
-async function bootstrap() {
-  const chassis = new Chassis([
-    resolve(__dirname, './ViewSpec.ts'),
-    resolve(__dirname, './ResolverSpec.ts')
-  ])
-
-  // Validate
-  chassis.validateSpec(resolve(__dirname, '../example/source.json'))
-
-  // Get all json schema
-  console.log(JSON.stringify(await chassis.generateJsonSchemaFile(), null, 2))
-}
-
-bootstrap()
-
-
-export type {
-  ChassisViewSpec,
-  ChassisResolverSpec
-}
+export type { ChassisViewSpec, ChassisResolverSpec }
