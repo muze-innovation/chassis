1d49a70dea71bd8aff3a10ff6cf6ec611e50a253
thain.s muze <thain.s@muze.co.th>
2023-02-01 14:21:10 +0700

    prepare debug function

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
-
+import { resolve } from 'path'
 import Chassis from './handler/Chassis'
 import { ChassisViewSpec } from '../src/spec/ChassisViewSpec'
 import { ChassisResolverSpec } from '../src/spec/ChassisResolverSpec'
 
 export default Chassis
 
+// function bootstrap() {
+//   const chassis = new Chassis([resolve('../example/src/ViewSpec.ts'), resolve('../example/src/ResolverSpec.ts')])
+
+//   // execute function what you want
+// }
+
+
+// bootstrap()
+
 export type { ChassisViewSpec, ChassisResolverSpec }
+
