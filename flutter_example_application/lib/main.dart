2bf4faad86b916b60e462d442f67217d5e51892c
nattaritk <nattarit.k@muze.co.th>
2023-02-01 18:09:16 +0700

    feat: initial firebase

--- flutter_example_application/lib/main.dart
+++ flutter_example_application/lib/main.dart
@@ -1,19 +1,62 @@
 import 'dart:async';
 
 import 'package:chassis/chassis.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:data_provider/data_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:view_provider/view_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_example_application/screens/screens.dart';
 
-void main() {
+// void main() {
+//   // setup chassis
+//   final dataProvider = DataProvider();
+//   final viewProvider = ViewProvider();
+//   Chassis.setup(dataProvider: dataProvider, viewProvider: viewProvider);
+
+//   // run app
+//   runApp(const MyApp());
+// }
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   // setup chassis
   final dataProvider = DataProvider();
   final viewProvider = ViewProvider();
   Chassis.setup(dataProvider: dataProvider, viewProvider: viewProvider);
-
-  // run app
   runApp(const MyApp());
+  readDataFireStore();
+}
+
+class QuickAccessItem {
+  final List<Item>? item;
+
+  QuickAccessItem(this.item);
+  Map<String, dynamic> toJson() => {'item': item};
+
+  QuickAccessItem.fromJson(Map<String, dynamic> json) : item = json['name'];
+}
+
+class Item {
+  final String? title;
+  final String? asset;
+  Item(this.title, this.asset);
+  Map<String, dynamic> toJson() => {'title': title, 'asset': asset};
+  Item.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        asset = json['asset'];
+}
+
+void readDataFireStore() {
+  Stream documentStream = FirebaseFirestore.instance
+      .collection('quickAccessItem')
+      .doc('C31m6JDhRAkqItIzWsKP')
+      .snapshots();
+  documentStream.listen((event) {
+    event as DocumentSnapshot;
+    // event.data() as Map<String, dynamic>;
+    print("documentStream: ${event.data() as Map<String, dynamic>}");
+  });
 }
 
 class MyApp extends StatelessWidget {
